9492d3d1b05575240f740feede606a0d1391918f
Michael Nairn <michael.nairn@feedhenry.com>
2015-05-08 19:36:13 +0100

    Set mongo gem version to 1.11.1

--- recipes/mongo_gem.rb
+++ recipes/mongo_gem.rb
@@ -2,10 +2,12 @@
 if(Gem.const_defined?("Version") and Gem::Version.new(Chef::VERSION) < Gem::Version.new('10.12.0'))
   gem_package 'mongo' do
     action :nothing
+    version "1.11.1"
   end.run_action(:install)
   Gem.clear_paths
 else
   chef_gem 'mongo' do
     action :install
+    version "1.11.1"
   end
 end
